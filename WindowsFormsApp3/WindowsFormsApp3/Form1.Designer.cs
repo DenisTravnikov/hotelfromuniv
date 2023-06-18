ed224c4e84bfd30ec5c53fd100798859cab5ab37
DenisTravnikov <136979885+DenisTravnikov@users.noreply.github.com>
2023-06-18 17:05:25 +0300

    Add files via upload

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.VIEW_OK = new System.Windows.Forms.Button();
+            this.VIEW_AV = new System.Windows.Forms.Button();
+            this.CheckLn = new System.Windows.Forms.Button();
+            this.CheckOut = new System.Windows.Forms.Button();
+            this.roomsListBox = new System.Windows.Forms.ListBox();
+            this.occupiedRoomsListBox = new System.Windows.Forms.ListBox();
+            this.availableRoomsListBox = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.occupied = new System.Windows.Forms.TabPage();
+            this.available = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // VIEW_OK
+            // 
+            this.VIEW_OK.Location = new System.Drawing.Point(155, 321);
+            this.VIEW_OK.Name = "VIEW_OK";
+            this.VIEW_OK.Size = new System.Drawing.Size(92, 31);
+            this.VIEW_OK.TabIndex = 0;
+            this.VIEW_OK.Text = "VIEW_OK";
+            this.VIEW_OK.UseVisualStyleBackColor = true;
+            this.VIEW_OK.Click += new System.EventHandler(this.VIEW_OK_Click);
+            // 
+            // VIEW_AV
+            // 
+            this.VIEW_AV.Location = new System.Drawing.Point(295, 321);
+            this.VIEW_AV.Name = "VIEW_AV";
+            this.VIEW_AV.Size = new System.Drawing.Size(85, 31);
+            this.VIEW_AV.TabIndex = 1;
+            this.VIEW_AV.Text = "VIEW_AV";
+            this.VIEW_AV.UseVisualStyleBackColor = true;
+            this.VIEW_AV.Click += new System.EventHandler(this.VIEW_AV_Click);
+            // 
+            // CheckLn
+            // 
+            this.CheckLn.Location = new System.Drawing.Point(430, 321);
+            this.CheckLn.Name = "CheckLn";
+            this.CheckLn.Size = new System.Drawing.Size(88, 31);
+            this.CheckLn.TabIndex = 4;
+            this.CheckLn.Text = "CheckLn";
+            this.CheckLn.UseVisualStyleBackColor = true;
+            this.CheckLn.Click += new System.EventHandler(this.CheckLn_Click);
+            // 
+            // CheckOut
+            // 
+            this.CheckOut.Location = new System.Drawing.Point(610, 321);
+            this.CheckOut.Name = "CheckOut";
+            this.CheckOut.Size = new System.Drawing.Size(89, 31);
+            this.CheckOut.TabIndex = 5;
+            this.CheckOut.Text = "CheckOut";
+            this.CheckOut.UseVisualStyleBackColor = true;
+            this.CheckOut.Click += new System.EventHandler(this.CheckOut_Click);
+            // 
+            // roomsListBox
+            // 
+            this.roomsListBox.FormattingEnabled = true;
+            this.roomsListBox.ItemHeight = 16;
+            this.roomsListBox.Location = new System.Drawing.Point(78, 164);
+            this.roomsListBox.Name = "roomsListBox";
+            this.roomsListBox.Size = new System.Drawing.Size(218, 84);
+            this.roomsListBox.TabIndex = 6;
+            // 
+            // occupiedRoomsListBox
+            // 
+            this.occupiedRoomsListBox.FormattingEnabled = true;
+            this.occupiedRoomsListBox.ItemHeight = 16;
+            this.occupiedRoomsListBox.Location = new System.Drawing.Point(315, 164);
+            this.occupiedRoomsListBox.Name = "occupiedRoomsListBox";
+            this.occupiedRoomsListBox.Size = new System.Drawing.Size(227, 84);
+            this.occupiedRoomsListBox.TabIndex = 7;
+            this.occupiedRoomsListBox.SelectedIndexChanged += new System.EventHandler(this.occupiedRoomsListBox_SelectedIndexChanged);
+            // 
+            // availableRoomsListBox
+            // 
+            this.availableRoomsListBox.FormattingEnabled = true;
+            this.availableRoomsListBox.ItemHeight = 16;
+            this.availableRoomsListBox.Location = new System.Drawing.Point(564, 164);
+            this.availableRoomsListBox.Name = "availableRoomsListBox";
+            this.availableRoomsListBox.Size = new System.Drawing.Size(203, 84);
+            this.availableRoomsListBox.TabIndex = 8;
+            this.availableRoomsListBox.SelectedIndexChanged += new System.EventHandler(this.availableRoomsListBox_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(353, 96);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.occupied);
+            this.tabControl1.Controls.Add(this.available);
+            this.tabControl1.Location = new System.Drawing.Point(78, 37);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(200, 100);
+            this.tabControl1.TabIndex = 10;
+            // 
+            // occupied
+            // 
+            this.occupied.Location = new System.Drawing.Point(4, 25);
+            this.occupied.Name = "occupied";
+            this.occupied.Padding = new System.Windows.Forms.Padding(3);
+            this.occupied.Size = new System.Drawing.Size(192, 71);
+            this.occupied.TabIndex = 0;
+            this.occupied.Text = "occupiedRoomsTabPage";
+            this.occupied.UseVisualStyleBackColor = true;
+            // 
+            // available
+            // 
+            this.available.Location = new System.Drawing.Point(4, 25);
+            this.available.Name = "available";
+            this.available.Padding = new System.Windows.Forms.Padding(3);
+            this.available.Size = new System.Drawing.Size(192, 71);
+            this.available.TabIndex = 1;
+            this.available.Text = "availableRoomsTabPage";
+            this.available.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.availableRoomsListBox);
+            this.Controls.Add(this.occupiedRoomsListBox);
+            this.Controls.Add(this.roomsListBox);
+            this.Controls.Add(this.CheckOut);
+            this.Controls.Add(this.CheckLn);
+            this.Controls.Add(this.VIEW_AV);
+            this.Controls.Add(this.VIEW_OK);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button VIEW_OK;
+        private System.Windows.Forms.Button VIEW_AV;
+        private System.Windows.Forms.Button CheckLn;
+        private System.Windows.Forms.Button CheckOut;
+        private System.Windows.Forms.ListBox roomsListBox;
+        private System.Windows.Forms.ListBox occupiedRoomsListBox;
+        private System.Windows.Forms.ListBox availableRoomsListBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage occupied;
+        private System.Windows.Forms.TabPage available;
+    }
+}
+
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private List<Room> rooms;
+        private List<Client> clients;
+        private object occupiedRoomsTabPage;
+        private object availableRoomsTabPage;
+
+        public Form1()
+        {
+            InitializeComponent();
+            rooms = new List<Room>();
+            clients = new List<Client>();
+
+            // Додати деякі тестові дані про номери готелю
+            rooms.Add(new Room("101", RoomClass.Standard));
+            rooms.Add(new Room("202", RoomClass.SemiLuxury));
+            rooms.Add(new Room("303", RoomClass.Luxury));
+            DesignForm();
+
+        }
+        private void DesignForm()
+        {
+            // Зміна дизайну кнопок
+            VIEW_OK.BackColor = Color.Blue;
+            VIEW_OK.ForeColor = Color.White;
+            VIEW_OK.Font = new Font("Arial", 12, FontStyle.Bold);
+
+            VIEW_AV.BackColor = Color.Green;
+            VIEW_AV.ForeColor = Color.White;
+            VIEW_AV.Font = new Font("Arial", 12, FontStyle.Bold);
+
+            CheckLn.BackColor = Color.Red;
+            CheckLn.ForeColor = Color.White;
+            CheckLn.Font = new Font("Arial", 12, FontStyle.Bold);
+
+            CheckOut.BackColor = Color.Orange;
+            CheckOut.ForeColor = Color.White;
+            CheckOut.Font = new Font("Arial", 12, FontStyle.Bold);
+
+            // Зміна дизайну текстового поля
+            textBox1.BackColor = Color.LightYellow;
+            textBox1.Font = new Font("Arial", 12);
+
+            // Зміна дизайну лістбоксів
+            roomsListBox.BackColor = Color.White;
+            roomsListBox.Font = new Font("Arial", 12);
+
+            occupiedRoomsListBox.BackColor = Color.White;
+            occupiedRoomsListBox.Font = new Font("Arial", 12);
+
+            availableRoomsListBox.BackColor = Color.White;
+            availableRoomsListBox.Font = new Font("Arial", 12);
+
+          
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            RefreshRoomList();
+        }
+
+        private void RefreshRoomList()
+        {
+            roomsListBox.Items.Clear();
+
+            foreach (var room in rooms)
+            {
+                roomsListBox.Items.Add($"{room.Number} ({room.RoomClass}) - {(room.IsOccupied ? "Зайнятий" : "Вільний")}");
+            }
+        }
+
+        private void VIEW_OK_Click(object sender, EventArgs e)
+        {
+            occupiedRoomsListBox.Items.Clear();
+
+            foreach (var room in rooms)
+            {
+                if (room.IsOccupied)
+                {
+                    occupiedRoomsListBox.Items.Add($"{room.Number} ({room.RoomClass})");
+                }
+            }
+
+            tabControl1.SelectedTab = occupied;
+        }
+
+        private void VIEW_AV_Click(object sender, EventArgs e)
+        {
+            availableRoomsListBox.Items.Clear();
+
+            foreach (var room in rooms)
+            {
+                if (!room.IsOccupied)
+                {
+                    availableRoomsListBox.Items.Add($"{room.Number} ({room.RoomClass})");
+                }
+            }
+
+            tabControl1.SelectedTab = available;
+        }
+
+        private void CheckLn_Click(object sender, EventArgs e)
+        {
+            string roomNumber = availableRoomsListBox.SelectedItem?.ToString().Split(' ')[0];
+            if (roomNumber != null)
+            {
+                var clientName = textBox1.Text;
+
+                if (!string.IsNullOrEmpty(clientName))
+                {
+                    rooms.Find(r => r.Number == roomNumber).IsOccupied = true;
+                    clients.Add(new Client(clientName, roomNumber));
+
+                    MessageBox.Show($"Клієнт {clientName} успішно заселений в номер {roomNumber}", "Заселення клієнта", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    RefreshRoomList();
+                    textBox1.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show("Введіть ім'я клієнта", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Виберіть вільний номер для заселення", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CheckOut_Click(object sender, EventArgs e)
+        {
+            string roomNumber = occupiedRoomsListBox.SelectedItem?.ToString().Split(' ')[0];
+            if (roomNumber != null)
+            {
+                var client = clients.Find(c => c.RoomNumber == roomNumber);
+
+                rooms.Find(r => r.Number == roomNumber).IsOccupied = false;
+                clients.Remove(client);
+
+                MessageBox.Show($"Клієнт {client.Name} успішно виселений з номера {roomNumber}", "Виселення клієнта", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                RefreshRoomList();
+            }
+            else
+            {
+                MessageBox.Show("Виберіть зайнятий номер для виселення", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void occupiedRoomsListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void availableRoomsListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Room
+    {
+        public string Number { get; set; }
+        public RoomClass RoomClass { get; set; }
+        public bool IsOccupied { get; set; }
+
+        public Room(string number, RoomClass roomClass)
+        {
+            Number = number;
+            RoomClass = roomClass;
+            IsOccupied = false;
+        }
+    }
+
+    public enum RoomClass
+    {
+        Standard,
+        SemiLuxury,
+        Luxury
+    }
+
+    public class Client
+    {
+        public string Name { get; set; }
+        public string RoomNumber { get; set; }
+        public Client(string name, string roomNumber)
+        {
+            Name = name;
+            RoomNumber = roomNumber;
+        }
+    }
+}
+
+
+
+
